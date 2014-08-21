d9419c483b3c34ad51e77c3b8e138f9389494347
James Ward <james@jamesward.org>
2014-08-21 11:19:05 -0600

    endpoint for salesforce outbound messages

--- app/controllers/ifttt/v1/Webhooks.scala
+++ app/controllers/ifttt/v1/Webhooks.scala
@@ -1,5 +1,7 @@
 package controllers.ifttt.v1
 
+import java.net.URL
+
 import play.api.Play
 import play.api.libs.json.Json
 import play.api.libs.ws.WS
@@ -38,6 +40,50 @@ object Webhooks extends Controller {
     }
     else {
       Future.successful(Ok)
+    }
+  }
+
+  def customSalesforceTrigger() = Action.async(parse.xml) { request =>
+
+    val sessionId = (request.body \ "Body" \ "notifications" \ "SessionId").text
+
+    val partnerUrl = new URL((request.body \ "Body" \ "notifications" \ "PartnerUrl").text)
+
+    val actionId = (request.body \ "Body" \ "notifications" \ "ActionId").text
+
+    val objectType = (request.body \ "Body" \ "notifications" \ "Notification" \ "sObject" \@ "{http://www.w3.org/2001/XMLSchema-instance}type").stripPrefix("sf:")
+
+    val objectId = (request.body \ "Body" \ "notifications" \ "Notification" \ "sObject" \ "Id").text
+
+    val restBaseUrl = s"https://${partnerUrl.getHost}/services/data/v31.0"
+
+    // get the workflow info
+    WS.url(s"$restBaseUrl/tooling/sobjects/WorkflowOutboundMessage/$actionId").withHeaders(AUTHORIZATION -> s"Bearer $sessionId").get().flatMap { actionResponse =>
+
+      val actionName = (actionResponse.json \ "Name").as[String]
+
+      // add a new record to ifttt events
+      val eventJson = Json.obj(
+        "ifttt__Type__c" -> actionName,
+        "Name" -> actionName,
+        "ifttt__Message__c" -> s"Details: https://${partnerUrl.getHost}/$objectId"
+      )
+
+      WS.url(s"$restBaseUrl/sobjects/ifttt__IFTTT_Event__c").withHeaders(AUTHORIZATION -> s"Bearer $sessionId").post(eventJson).map { createEventResponse =>
+
+        val success = (createEventResponse.json \ "success").as[Boolean]
+
+        // ack the notification
+        Ok(s"""
+             |<soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
+             |    <soapenv:Body>
+             |        <notificationsResponse xmlns="http://soap.sforce.com/2005/09/outbound">
+             |            <Ack>$success</Ack>
+             |        </notificationsResponse>
+             |    </soapenv:Body>
+             |</soapenv:Envelope>
+           """.stripMargin).as("text/xml")
+      }
     }
   }
 
