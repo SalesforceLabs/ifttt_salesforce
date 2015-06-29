e442e7d9f553b20d56480e373045d208856d56f7
James Ward <james@jamesward.org>
2015-06-29 15:10:51 -0600

    externalize the managed package id

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,11 +1,14 @@
 package controllers
 
+import play.api.Play
 import play.api.mvc.{Action, Controller}
 
 object Application extends Controller {
 
+  lazy val managedPackageId = Play.current.configuration.getString("salesforce.managed-package-id").get
+
   def index() = Action {
-    Ok(views.html.index())
+    Ok(views.html.index(managedPackageId))
   }
 
 }
